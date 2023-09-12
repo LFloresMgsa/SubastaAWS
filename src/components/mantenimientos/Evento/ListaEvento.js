addaac4cb1c5d0d6c3cdacaf337b0a6777b5bcaf
MANAGEMENT GROUP SA <mlopez@mgsa.com.pe>
2023-09-11 21:15:50 -0500

    finish

--- src/components/mantenimientos/Evento/ListaEvento.js
+++ src/components/mantenimientos/Evento/ListaEvento.js
@@ -144,7 +144,7 @@ const ListaEvento = (props) => {
                       <TableHead>
                         <TableRow>
 
-                          <StyledTableCell align="right">Llave</StyledTableCell>
+                          <StyledTableCell align="left">Llave</StyledTableCell>
                           <StyledTableCell align="left">Tipo</StyledTableCell>
                           <StyledTableCell align="left">Descripción</StyledTableCell>
                           <StyledTableCell align="left">Inicio</StyledTableCell>
@@ -163,16 +163,16 @@ const ListaEvento = (props) => {
                         {data.map((item, idx) => (
                           <StyledTableRow item={item} key={`${item.Emp_cCodigo}-${item.Llave}`}>
 
-                            <StyledTableCell align="right">{item.Llave}</StyledTableCell>
+                            <StyledTableCell align="left">{item.Llave}</StyledTableCell>
                             <StyledTableCell align="left">{item.Vtt_cTipoEvento}</StyledTableCell>
-                            <StyledTableCell align="center">{item.Dvm_cDescripcion}</StyledTableCell>
+                            <StyledTableCell align="left">{item.Dvm_cDescripcion}</StyledTableCell>
                             <StyledTableCell align="left">{item.Dvm_dInicio}</StyledTableCell>
                             <StyledTableCell align="left">{item.Dvm_dFin}</StyledTableCell>
                             <StyledTableCell align="left">{item.Dvm_cEstado}</StyledTableCell>
 
                             <StyledTableCell align="left">{item.Dvm_cDescripcionSec}</StyledTableCell>
-                            <StyledTableCell align="left">{ccyFormat(item.Dvm_nTopeImporte)}</StyledTableCell>
-                            <StyledTableCell align="left">{ccyFormat(item.Dvm_nTopeDias)}</StyledTableCell>
+                            <StyledTableCell align="right">{ccyFormat(item.Dvm_nTopeImporte)}</StyledTableCell>
+                            <StyledTableCell align="right">{ccyFormat(item.Dvm_nTopeDias)}</StyledTableCell>
 
                             <StyledTableCell align="left"><Button variant="contained" size="small" color="primary" onClick={() => editar(item.Emp_cCodigo, item.Pan_cAnio, item.Per_cPeriodo, item.Dvm_cNummov)} >Editar</Button></StyledTableCell>
                             {/* <StyledTableCell align="left"><Button variant="contained" size="small" color="primary" onClick={() => eliminar(item.Emp_cCodigo, item.Pan_cAnio, item.Per_cPeriodo, item.Dvm_cNummov)} >Eliminar</Button></StyledTableCell> */}
